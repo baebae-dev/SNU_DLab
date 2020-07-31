b2db8d56fe4a48794d50ca0c279913704a2336d6
baeyuna <baeyuna@MacBook-Pro.local>
2020-07-31 13:37:00 +0900

    msn_crawler

--- README.md
+++ README.md
@@ -9,14 +9,12 @@
 | Task |  부연설명 |  참조 사이트 및 자료 |
 |------|---------|------------------|
 | Data Crawler | 데이터 추가수집 | [MIND] https://github.com/msnews/MIND |
+| Data Crawler | 크롤러 생성 | [msn_crawler] /SNU_Dlab/msn |
 
 ```
 * Scrapy Crawler *
- |_ Crawler  
-   |_ MIND  
-     |_ crawler  
-       |_ msn  
-         |_ pipeline (데이터 저장 방식 지정 파일)  
+
+
 ```
 
 
---
+++ msn/msn/spiders/msn.py
@@ -0,0 +1,17 @@
+import scrapy
+from msn.items import MsnItem
+
+class MsnSpider(scrapy.Spider):
+    name = 'msn'
+    allowed_domains = ['https://www.msn.com/en-us/news/world/chile-three-die-in-supermarket-fire-amid-protests/ar-AAJ43pw?ocid=chopendata']
+    start_urls = ['https://www.msn.com/en-us/news/world/chile-three-die-in-supermarket-fire-amid-protests/ar-AAJ43pw?ocid=chopendata/']
+
+    def parse(self, response):
+        titles = response.css('header.collection-headline-flex > span > h1::text').get()         
+        # dates = response.css('selection > div.authorinfo-flexar > div > div > span > time::text').get()
+        # images = response.css('article > section.flexarticle > section.articlebody >  span.image > img::attr("src")').get() 
+
+        for num, title in enumerate(titles):
+            item = MsnItem() # 클래스로 객체 생성 
+            item['title'] = title # item에 넣어라 
+            yield item # yield 할때마다 items.py의 item 객체에 데이터가 쌓인다.
